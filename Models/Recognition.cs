92c865e18dca08f2a2fee3f7dfffd264571ebb23
mk792418 <mk792418@ohio.edu>
2022-03-02 11:27:14 -0500

    Recongized Model

---
+++ Models/Recognition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISTeam8.Models
+{
+     public class Recognition
+    {
+        [Required]
+        [Display(Name = "Recongizer" )]
+        public string RecongizerID { get; set; }
+        [Required]
+        [Display(Name ="Recongized")]
+        public string RecongizedID { get; set; }
+        [Required]
+        [Display(Name = "Core Value")]
+
+        public string CoreValue { get; set; }
+        [Required]
+        [Display(Name = "Date Recongized")]
+        public DateTime DateRecongized { get; set; }
+        [Required]
+        public string Details { get; set; }
+        
+
+    }
+}
+
